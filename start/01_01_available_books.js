78cc7b9b345a9e564267139fe0d70c09e99c3635
Lucy W. Mwangi <100368317+lucy-sees@users.noreply.github.com>
2023-09-18 14:06:58 +0300

    Use class component

--- start/01_01_available_books.js
+++ start/01_01_available_books.js
@@ -1,28 +1,57 @@
 // Write your code here
-function Book(title, author, ISBN, numCopies) {
-  this.title = title;
-  this.author = author;
-  this.ISBN = ISBN;
-  this.numCopies = numCopies;
-}
+// function Book(title, author, ISBN, numCopies) {
+//   this.title = title;
+//   this.author = author;
+//   this.ISBN = ISBN;
+//   this.numCopies = numCopies;
+// }
 
-Book.prototype.getAvailability = function () {
-  if (this.numCopies === 0){
-    return "Out of stock";
-  }else if (this.numCopies < 10) {
-    return "Low stock";
+// Book.prototype.getAvailability = function () {
+//   if (this.numCopies === 0){
+//     return "Out of stock";
+//   }else if (this.numCopies < 10) {
+//     return "Low stock";
+//   }
+//   return "In stock";
+// }
+
+// Book.prototype.sell = function (numCopiesSold = 1) {
+//   this.numCopies -= numCopiesSold;
+// }
+
+// Book.prototype.restock = function (numCopiesStocked = 5) {  
+//   this.numCopies += numCopiesStocked;
+// }
+
+//use constructor (class component)
+class Book {
+  constructor(title, author, ISBN, numCopies) {
+    this.title = title;
+    this.author = author;
+    this.ISBN = ISBN;
+    this.numCopies =numCopies;
   }
-  return "In stock";
-}
+  get availability() {
+    return this.getAvailability();
+  }
+  //encapsulation
+  getAvailability() {
+    if (this.numCopies === 0){
+      return "Out of stock";
+    }else if (this.numCopies < 10) {
+      return "Low stock";
+    }
+    return "In stock";
+  }
 
-Book.prototype.sell = function (numCopiesSold = 1) {
-  this.numCopies -= numCopiesSold;
-}
+  sell(numCopiesSold = 1){
+    this.numCopies -= numCopiesSold;
+  }
 
-Book.prototype.restock = function (numCopiesStocked = 5) {  
-  this.numCopies += numCopiesStocked;
+  restock(numCopiesStocked = 5){
+    this.numCopies += numCopiesStocked;
+  }
 }
-
 const HungerGames = new Book("Hunger Games", "Suzanne Collins", 123919, 5)
 console.log(HungerGames.getAvailability());
 
