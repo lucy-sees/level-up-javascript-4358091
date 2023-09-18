9c98b50d4d309600d9bcffb1196a87ab86b55fa4
Lucy W. Mwangi <100368317+lucy-sees@users.noreply.github.com>
2023-09-18 17:04:13 +0300

    This will log output in the console

--- start/01_05_private_object.js
+++ start/01_05_private_object.js
@@ -1,1 +1,12 @@
 // Write your code here
+const username = Symbol("username");
+const password = Symbol("password");
+
+const user = {
+  username: "John",
+  password: "12345",
+  age: 25
+};
+
+console.log(user.username); // John
+console.log(password) //12345
