262bcbabc4634497d47340bd793f6b8af3e86d36
Lucy W. Mwangi <100368317+lucy-sees@users.noreply.github.com>
2023-09-18 17:19:50 +0300

    Apply destructing syntax

--- start/01_07_classroom_attendance.js
+++ start/01_07_classroom_attendance.js
@@ -1,1 +1,17 @@
-// Write your code here
+// destructuring
+function getStudents(classroom) {
+  let { hasTeachingAssistant, classList } = classroom;
+  let teacher, teachingAssistant, students;
+
+  if(hasTeachingAssistant) {
+    [teacher,teachingAssistant, students] = classList;
+  } else {
+    [teacher, ...students] = classList;
+  }
+  return students;
+}
+
+console.log(getStudents({
+  hasTeachingAssistant: false,
+  classList: ["Raj", "Sheldon", "Howard", "Penny", "Amy"]
+}));
