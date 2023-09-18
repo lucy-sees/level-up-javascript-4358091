22129a1738715c2471ef0609aff54ef30b622783
Lucy W. Mwangi <100368317+lucy-sees@users.noreply.github.com>
2023-09-18 14:21:36 +0300

    Use function component

--- start/01_02_movie_prototype.js
+++ start/01_02_movie_prototype.js
@@ -1,1 +1,20 @@
-// Write your code here
+function Movie(title, director, genre,releaseYear, rating) {
+  this.title = title;
+  this.director = director;
+  this.genre = genre;
+  this.releaseYear = releaseYear;
+  this.rating = rating;
+}
+
+Movie.prototype.getOverview = function() {
+  return `${this.title}, a ${this.genre} film directed by ${this.director} was released in
+  ${this.releaseYear} and received a rating of ${this.rating}`;
+}
+
+const Spiderman = new Movie("Spiderman", "Sam Raimi", "Action", 2008, 87);
+const Batman = new Movie("Batman", "Christopher Nolan", "Action", 2008, 83);
+
+console.log(Spiderman);
+console.log(Spiderman.getOverview());
+console.log(Batman);
+console.log(Batman.getOverview());
