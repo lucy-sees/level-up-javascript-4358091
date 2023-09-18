e17d7c9f6aa60180c78e208ea6b462b8c6b009df
Lucy W. Mwangi <100368317+lucy-sees@users.noreply.github.com>
2023-09-18 17:05:42 +0300

    This will log undefined in the console

--- start/01_05_private_object.js
+++ start/01_05_private_object.js
@@ -3,10 +3,10 @@ const username = Symbol("username");
 const password = Symbol("password");
 
 const user = {
-  username: "John",
-  password: "12345",
+  [username]: "John",
+  [password]: "12345",
   age: 25
 };
 
-console.log(user.username); // John
-console.log(password) //12345
+console.log(user.username); // undefined
+console.log(user.password); //undefined
